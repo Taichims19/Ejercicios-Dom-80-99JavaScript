2593d22af80faec573f1b00a962feb932116a26c
Daniel Mendez <taichinura19@gmail.com>
2023-08-09 21:41:32 -0400

    Reloj de Cuenta Regresiva Add

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,9 @@
+import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
-import { shortcuts } from "../teclado.js";
+import { moveBall, shortcuts } from "./dom/teclado.js";
+
+
 
 const d = document;
 
@@ -8,9 +11,11 @@ d.addEventListener("DOMContentLoaded", (e)=>{
     hamburgerMenu(".panel-btn",".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/alarma-morning-mix.mp3","#activar-alarma", "#desactivar-alarma");
-
+    countdown("countdown","jul 18, 2024 20:55:19", "Feliz Cumpleaños Danny😎");
+    
 });
 
 d.addEventListener("keydown", (e)=>{
     shortcuts(e);
+    moveBall(e,".ball", ".stage");
 });
