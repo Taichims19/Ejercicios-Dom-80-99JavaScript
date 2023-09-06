d79cb79005bcba04603a9a44f8dc032c88817086
Daniel Mendez <taichinura19@gmail.com>
2023-09-05 22:28:22 -0400

    Filtro busquedas add

--- js/index_dom.js
+++ js/index_dom.js
@@ -10,6 +10,7 @@ import userDeviceInfo from "./dom/deteccion_dispositivos.js";
 import networkStatus from "./dom/deteccion_red.js";
 import webCam from "./dom/deteccion_webcam.js";
 import getGeolocation from "./dom/geolocalizacion.js";
+import searchFilter from "./dom/filtro_busquedas.js";
 
 
 
@@ -40,6 +41,7 @@ d.addEventListener("DOMContentLoaded", (e)=>{
   userDeviceInfo("user-device");
   webCam("webcam");
   getGeolocation("geolocation");
+  searchFilter(".card-filter",".card");
   
 });
 
