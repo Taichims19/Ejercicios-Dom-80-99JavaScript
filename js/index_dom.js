3f4c3aed7bfe671223b18d11d729585bf13916d9
Daniel Mendez <taichinura19@gmail.com>
2023-09-17 12:18:46 -0400

    Formulario y validaciones y envio basico add

--- js/index_dom.js
+++ js/index_dom.js
@@ -15,6 +15,7 @@ import draw from "./dom/sorteo.js";
 import slider from "./dom/carrusel.js";
 import scrollSpy from "./dom/scroll_espia.js";
 import smartVideo from "./dom/video_inteligente.js";
+import contactFormValidations from "./dom/validaciones_formulario.js";
 
 
 
@@ -50,6 +51,7 @@ d.addEventListener("DOMContentLoaded", (e)=>{
   slider();
   scrollSpy();
   smartVideo();
+  contactFormValidations();
   
 });
 
