26cbcb431077e2e2d199a1c3e49afb4fd3b998ef
Daniel Mendez <taichinura19@gmail.com>
2023-10-12 14:43:18 -0400

    Carpetas añadidas de Javascript

--- js/index_dom.js
+++ js/index_dom.js
@@ -16,6 +16,7 @@ import slider from "./dom/carrusel.js";
 import scrollSpy from "./dom/scroll_espia.js";
 import smartVideo from "./dom/video_inteligente.js";
 import contactFormValidations from "./dom/validaciones_formulario.js";
+import speechReader from "./dom/narrador.js";
 
 
 
@@ -62,3 +63,4 @@ d.addEventListener("keydown", (e)=>{
 
 darkTheme(".dark-theme-btn", "dark-mode");
 networkStatus();
+speechReader();
